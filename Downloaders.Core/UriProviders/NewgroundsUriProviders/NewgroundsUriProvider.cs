8c714eb92be17f70bf485621a415cc9699803193
ElrohirGT <elrohirgt@gmail.com>
2021-10-26 23:02:32 -0600

    Added a limit to only have one active browser at a time.

--- Downloaders.Core/UriProviders/NewgroundsUriProviders/NewgroundsUriProvider.cs
+++ Downloaders.Core/UriProviders/NewgroundsUriProviders/NewgroundsUriProvider.cs
@@ -1,7 +1,6 @@
-﻿using CefSharp;
-using CefSharp.OffScreen;
+﻿using CefSharp.OffScreen;
 using System;
-using System.IO;
+using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
@@ -9,31 +8,27 @@ namespace Downloaders.Core.UriProviders.NewgroundsUriProviders
 {
     internal sealed class NewgroundsUriProvider : BaseResourceUriProvider
     {
-        public NewgroundsUriProvider()
-        {
-            var settings = new CefSettings
-            {
-                LogSeverity = LogSeverity.Disable,
-                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
-            };
-            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
-        }
+        private const int MAX_BROWSERS_CONCURRENTLY = 1;
+        private static readonly SemaphoreSlim _semaphore = new SemaphoreSlim(MAX_BROWSERS_CONCURRENTLY);
 
         public override Task<int> GetNumberOfItems(Uri uri) => Task.FromResult(1);
 
         public override async Task GetUris(Uri uri, string mainPath, ChannelWriter<DownloadableFile> writer)
         {
-            using var browser = new ChromiumWebBrowser(uri.AbsoluteUri);
-            INewgroundsFileProvider provider = await NewgroundsFileUriProviderFactory.GetProvider(browser).ConfigureAwait(false);
-            DownloadableFile file = await provider.GetFile().ConfigureAwait(false);
-            await writer.WriteAsync(file).ConfigureAwait(false);
-        }
+            try
+            {
+                await _semaphore.WaitAsync().ConfigureAwait(false);
 
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-            if (disposing)
-                Cef.Shutdown();
+                using var browser = new ChromiumWebBrowser(uri.AbsoluteUri);
+                INewgroundsFileProvider provider = await NewgroundsFileUriProviderFactory.GetProvider(browser).ConfigureAwait(false);
+
+                DownloadableFile file = await provider.GetFile().ConfigureAwait(false);
+                await writer.WriteAsync(file).ConfigureAwait(false);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
     }
 }
